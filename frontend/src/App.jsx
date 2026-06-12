bf8be9142461ccb8ff39821c3fd0142f820eccdb
Pratham Vyas <prathamrvyas@gmail.com>
2026-06-12 23:54:51 +0530

    Getting reply from prompt in console and also adding a loading spinner till assistant response arrives

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,22 @@ import Sidebar from './Sidebar.jsx';
 import ChatWindow from './ChatWindow.jsx'
 import Chat from './Chat.jsx'
 import { MyContext } from './MyContext.jsx';
+import { useState } from 'react';
+import {v1 as uuidv1} from "uuid";
 
 function App() {
-  const providerValues = {};
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId
+  };
 
   return (
     <div className='main'>
-      <MyContext.Provider values = {providerValues}>
+      <MyContext.Provider value= {providerValues}>
         <Sidebar></Sidebar>
         <ChatWindow></ChatWindow>
       </MyContext.Provider>
--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,26 +1,63 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext, useState } from "react";
+import {ScaleLoader} from "react-spinners";
+
 
 function ChatWindow(){
+  
+  const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+  const [loading, setLoading] = useState(false);
+
+  const getReply = async () =>{
+    setLoading(true);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId: currThreadId
+      })
+    };
+     try{
+       const response = await fetch("http://localhost:8080/api/chat", options);
+       const res = await response.json();
+       console.log(res);
+       setReply(res.reply);
+     }catch(err){
+      console.log(err)
+     }
+     setLoading(false);
+  }
+
+
   return (
     <div className="chatWindow">
 
       <div className="navbar">
-        <span>MyGPT <i class="fa-solid fa-angle-down"></i></span>
+        <span>MyGPT <i className="fa-solid fa-angle-down"></i></span>
         <div className="userIconDiv">
-          <span className="userIcon"><i class="fa-regular fa-user"></i></span>
+          <span className="userIcon"><i className="fa-regular fa-user"></i></span>
           </div>
       </div>  
 
       <Chat></Chat>
+      <ScaleLoader color="#fff" loading={loading}>
 
+      </ScaleLoader>
       <div className="chatInput">
         <div className="inputBox">
-          <input placeholder="Ask anything">
-
+          <input placeholder="Ask anything"
+          value = {prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                onKeyDown={(e) => e.key === 'Enter'? getReply(): ''}>
+                
           </input>
-         <div id="submit">
-          <i class="fa-regular fa-paper-plane"></i>
+         <div id="submit" onClick={getReply}>
+          <i className="fa-regular fa-paper-plane"></i>
          </div>
         </div>
 
